8d98a1232095053eef812a1334132cba33f14c23
Raiver0f <r1ver0ffundm4nt@gmail.com>
2024-09-28 01:34:42 +0200

    change build path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,4 +10,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "../backend/src/main/resourses/static",
+    emptyOutDir: true, // also necessary
+  },
 });
